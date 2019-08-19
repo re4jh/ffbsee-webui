b9de1743caa6f289422f179a68eada15299b4338
Jonas Hess <jonas.hess@mailbox.org>
2019-08-19 23:52:29 +0200

    moved upgrade stuff to scripts.js

--- lan/js/scripts.js
+++ lan/js/scripts.js
@@ -1181,6 +1181,43 @@ function save_data() {
 }
 
 /* eof: network-stuff */
+/* bof: upgrade-stuff */
+
+function restore_firmware() {
+	if (!confirm("Sollen alle Einstellungen zur\xFCckgesetzt werden?")) {
+		return;
+	}
+
+	send("/cgi-bin/upgrade", {
+		func: 'restore_firmware'
+	}, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_upgrade() {
+	setText('msg', 'Versuche Updateserver zu erreichen. Bitte warten ...');
+	send("/cgi-bin/upgrade", {
+		func: 'lookup_upgrade'
+	}, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_and_apply_upgrade() {
+	if (!confirm("Soll ein Update durchgef\xFChrt werden?")) {
+		return;
+	}
+
+	setText('msg', 'Versuche Updateserver zu erreichen. Bitte warten ...');
+	send("/cgi-bin/upgrade", {
+		func: 'lookup_and_apply_upgrade'
+	}, function(text) {
+		setText('msg', text);
+	});
+}
+
+/* eof: upgrade-stuff */
 /* bof: general-stuff */
 
 function section_toggle(section_selector) {
